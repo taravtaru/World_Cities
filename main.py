cacd7fca7d088d2e2d126207140a54f03ebd36d9
ArtamonovDanila <danila.art@list.ru>
2024-06-02 21:35:12 +0300

    cluster_viz

---
+++ main.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+all_x = []
+all_y = []
+f = open("city_clusters.txt", "r")
+while True:
+    s = f.readline()
+    if not s:
+        break
+    n = int(s)
+    cur_x = []
+    cur_y = []
+    for i in range(n):
+        coords = f.readline()
+        w = coords.split()
+        cur_x.append(float(w[0]))
+        cur_y.append(float(w[1]))
+    all_x.append(cur_x)
+    all_y.append(cur_y)
+f.close()
+
+for i in range(len(all_x)):
+    plt.scatter(all_x[i], all_y[i], s=1)
+plt.show()
